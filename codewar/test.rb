e2a8749c6344fb10e8e53b17d17e7b1e68be55ad
SheebaJ <ashasheebajnn1996@gmail.com>
2020-01-29 21:59:30 +0530

    5thlevl dne

--- codewar/test.rb
+++ codewar/test.rb
@@ -1,15 +1,19 @@
-# s1="abc"
-# x=((s1.size..s1.size).flat_map{|k| s1.split(//).permutation(k).to_a}).sort
-# p (x[(x.length/2)-1]).join("")
-
-def createPhoneNumber(numbers)
-    first = "" ; second = "" ; last = ""
-    0.upto(2) {|x| first += numbers[x].to_s}
-    3.upto(5) {|x| second += numbers[x].to_s}
-    6.upto(9) {|x| last += numbers[x].to_s}
-   p "'('#{first}')' #{second}-#{last}"
-  end
+# def caeser(message, key)
+#     if key == 0
+#     return message.upcase
+#     elsif key > 1
+#       @r=('A'..'Z').to_a.rotate(key).join
+#         return message.upcase.tr "A-Z", @r
+#     end
+#   end
 
 
-createPhoneNumber(Array[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
-# => returns"(123) 456-7890"
+# p  caeser("hello world", 0)
+  # "HELLO WORLD")
+# Test.assert_equals(
+#   p  caeser("hello", 7)
+z="AsEowq8[DSE[A"evmsb_`mz/@=]b:"{IFA1gN$Quw:T9cp&OaNNJWiyyZ%gJPo"
+
+#     # , "OLSSV")
+
+p z."^[A-Z!@#$&()-`.+,/\"]*$"
--- codewar/weight.rb
+++ codewar/weight.rb
@@ -2,25 +2,39 @@ def order_weight(strng)
     w = {}
     # p w
     war = []
-    s = strng.to_s.split(" ").sort
-    s.each_with_index do |x,i|
-      n = 0
-      x.split("").each do |y|
-        n += y.to_i
-        # p n
-      end
+    s = strng.to_s.split(" ")
+    # p s
+   s.each_with_index do |x,i|
+    # p "#{x} #{i}"
+       n = 0
+    x.split("").each do |y|
+         n += y.to_i
+       end
+      #  p n
       w[x] = n
     end
-    # p s
-    # p w
-    Hash[w.sort_by{|k, v| v}].each do |v,k|
-    #   puts "v=#{v}   k=#{k}"
-      war.push([k,v])
-    end
-    #  p war
-     z=Hash[w.sort_by{|k, v| w.values}]
-    #  p z
-     p z.keys.join(" ")
+    x=" "
+    (w.values.sort).each{|i| x+=w.key(i)+ " "}
+    p x[1..(x.size-2)]
+#     Hash[w.sort_by{|k, v| v}].each do |v,k|
+#     #   puts "v=#{v}   k=#{k}"
+#       war.push([k,v])
+#     end
+#     #  p war
+#      z=Hash[w{|k, v| w.values}]
+# p z
   end
   
- order_weight("103 123 4444 99 2000")
+ order_weight("2000 2000 10003 10003 123 1234000 44444444 9999")
+#  "11 11 2000 10003 22 123 1234000 44444444 9999")
+
+
+#  Test.describe("Order Weight") do
+#   Test.it("Basic tests") do
+#       Test.assert_equals(order_weight("103 123 4444 99 2000"), "2000 103 123 4444 99")
+#       Test.assert_equals(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"), "11 11 2000 10003 22 123 1234000 44444444 9999")
+#   end
+# end
+
+# Expected: "1 2 200 4 4 6 6 7 7 18 27 72 81 9 91 425 31064 7920 67407 96488 34608557 71899703",
+#  instead got: "1 200 200 4 6 7 9 9 9 9 9 91 425 31064 7920 67407 96488 34608557 71899703"
